2e2b5d6fae05b53cbe122c0be2db0efc648dd461
vlapugb <vladimirpugovkinspb23@yandex.ru>
2021-11-14 03:12:04 +0300

    practice finish_me finished

--- practice/finish_me/finish_me.py
+++ practice/finish_me/finish_me.py
@@ -3,7 +3,7 @@ def larger_than(a, x):
     Вернуть все элементы a, большие x
     larger_than([1, 8, 11], 4) -> [8, 11]
     """
-    return [b for b in a if ...]
+    return [b for b in a if b>x]
 
 
 def names_to_strings(names, surnames):
@@ -11,7 +11,7 @@ def names_to_strings(names, surnames):
     Вернуть массив полных имен по данным именам и фамилиям
     names_to_strings(['Alice', 'John'], ['Smith', 'Doe']) -> ['Alice Smith', 'John Doe']
     """
-    return [... for name, surname in ...(names, surnames)]
+    return [name+" "+surname for name, surname in zip(names, surnames)]
 
 
 def last_first(s):
@@ -20,8 +20,8 @@ def last_first(s):
     last_first('Hello') -> 'oellH'
     """
     if len(s) < 2:
-        return ...
-    return ... + s[1:...] + s[0]
+        return s
+    return s[-1] + s[1:-1] + s[0]
 
 
 def int_strings(a):
@@ -31,9 +31,9 @@ def int_strings(a):
     """
 
     def change_item(x):
-        return str(x) if isinstance(..., ...) else ...
+        return str(x) if isinstance(x, int) else int(x)
 
-    return list(map(..., a))
+    return list(map(change_item, a))
 
 
 def no_long_words(s):
@@ -42,4 +42,4 @@ def no_long_words(s):
     no_long_words('Hello beautiful world') -> 'Hello world'
     """
     words = s.split(' ')
-    return ' '.join([...])
+    return ' '.join([a for a in words if len(a) < 6])
