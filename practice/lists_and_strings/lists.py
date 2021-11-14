09f086a3c2627e062bfbce6325cb522e4c8b953e
vlapugb <vladimirpugovkinspb23@yandex.ru>
2021-11-14 03:16:10 +0300

    practice lists_and_strings finished

--- practice/lists_and_strings/lists.py
+++ practice/lists_and_strings/lists.py
@@ -3,7 +3,7 @@ def sum_some(numbers, indices):
     Вернуть сумму элементов из numbers, на позициях, записанных в indices
     sum_some([1, 2, 3], [0, 1, 1]) -> 5 = (1+2+2)
     """
-    pass
+    return sum([numbers[i] for i in indices])
 
 
 def replace_range(a, b, x, y):
@@ -11,7 +11,7 @@ def replace_range(a, b, x, y):
     Заменить подмассив массива a с индексами от x до y на массив b
     replace_range([1, 2, 3, 4], [7, 8, 9], 0, 1) -> [7, 8, 9, 3, 4]
     """
-    pass
+    return a[:x]+b+a[y+1:]
 
 
 def sum_list(a):
@@ -19,7 +19,16 @@ def sum_list(a):
     Рекурсивно просуммировать элементы массива a
     sum_list([1, 2, 3]) -> 6
     """
-    pass
+    def temp_func(a):
+        t=a[:-1]
+        t[-1]+=a[-1]
+        return t
+    if len(a)==0:
+        return 0
+    if len(a)==1:
+        return a[0]
+    else:
+        return sum_list(temp_func(a))
 
 
 def signs(a):
@@ -27,4 +36,14 @@ def signs(a):
     Расставьте между элементами массива a знаки отношений "меньше", "равно" или "больше"
     signs([2, 8, 4, 4]) -> [2, '<', 8, '>', 4, '=', 4]
     """
-    pass
+    temp_array = []
+    for i, el in enumerate(a[:-1]):
+        temp_array += [el]
+        if a[i + 1] > el:
+            temp_array += ["<"]
+        elif a[i + 1] < el:
+            temp_array += [">"]
+        else:
+            temp_array += ["="]
+    temp_array += [a[-1]]
+    return temp_array
--- practice/lists_and_strings/strings.py
+++ practice/lists_and_strings/strings.py
@@ -3,7 +3,7 @@ def reverse(s):
     Вернуть исходную строку, записанную наоборот
     reverse('abc') -> 'cba'
     """
-    pass
+    return s[::-1]
 
 
 def swap_case(s):
@@ -11,15 +11,14 @@ def swap_case(s):
     Заменить регистр каждого символа, входящего в строку, на противоположный
     swap_case('Hello world') -> 'hELLO WORLD'
     """
-    pass
-
+    return "".join([i.upper() if i.islower() else i.lower() for i in s])
 
 def censor_words(s, blacklist):
     """
     Вернуть исходную строку, в которой слова из blacklist заменены на звездочки
     censor_words('hello world', ['hello']) -> '***** world')
     """
-    pass
+    return ' '.join(["*" * len(a) if a in blacklist else a for a in s.split(' ')])
 
 
 def remove_duplicates(s):
@@ -27,7 +26,13 @@ def remove_duplicates(s):
     Удалить идущие подряд повторы слов в строке
     remove_duplicates('hello nice nice world hello') -> 'hello nice world hello'
     """
-    pass
+    temp_w = []
+    temp_str = ""
+    for i in s.split(" "):
+        if i != temp_str:
+            temp_w.append(i)
+        temp_str = i
+    return ' '.join(temp_w)
 
 
 def parentheses(s):
@@ -37,4 +42,16 @@ def parentheses(s):
     parentheses( '(lalala' )       -> False - скобка не закрыта
     parentheses( '(hello)world)' ) -> False - слишком много закрывающих скобок
     """
-    pass
+    def prove_function(a):
+        opened = a.count(")")
+        closed = a.count("(")
+        if closed > opened:
+            return False
+        return True
+
+    if s.count(")") == s.count("("):
+        for i in range(len(s)):
+            if not prove_function(s[:i]):
+                return False
+        return True
+    return False
